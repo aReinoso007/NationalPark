51da7a6b3032b49f0fbda06e9bb20f8b6cdd00a9
Alex Reinoso <ralexjessiel@gmail.com>
2023-10-19 21:42:57 -0500

    navigation

--- HelloWorld/Views/LandmarkList.swift
+++ HelloWorld/Views/LandmarkList.swift
@@ -9,17 +9,17 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        /*
-         Lists work with identifiable data
-         this can be done in 2 ways:
-         1. passing along data and a key path to a property that
-         uniquely identifies each element
-         2. making the data type conform to the Identifiable
-         protocol
-         */
-        List(landmarks){
-            landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationSplitView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                                    LandmarkDetail()
+                                } label: {
+                                    LandmarkRow(landmark: landmark)
+                                }
+            }
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
         }
     }
 }
