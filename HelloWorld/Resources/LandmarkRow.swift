dd6781b5c789553be1c28f8d6b7f07d2338a069b
Alex Reinoso <ralexjessiel@gmail.com>
2023-11-06 21:51:20 -0500

    show star if item is favorite

--- HelloWorld/Resources/LandmarkRow.swift
+++ HelloWorld/Resources/LandmarkRow.swift
@@ -21,6 +21,10 @@ struct LandmarkRow: View {
 
                     Spacer()
                 }
+        if landmark.isFavorite {
+            Image(systemName: "star.fill")
+                .foregroundColor(.yellow)
+        }
     }
 }
 
